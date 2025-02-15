a18c0cddc1d3169ad08efa31191ff0e31a8998f1
Sagenseele <ezimmermann18@googlemail.com>
2025-02-15 23:04:29 +0100

    changed quests, added sprites, added mod

--- kubejs/server_scripts/interactions.js
+++ kubejs/server_scripts/interactions.js
@@ -1,5 +1,48 @@
+
+
+// Dirt Essence
+
+BlockEvents.rightClicked('minecraft:dirt', event => {
+    if(event.hand == 'main_hand' && event.player.getMainHandItem().id == 'minecraft:air') {
+        event.player.swing(event.hand, true)
+        event.player.give('mysticalagriculture:dirt_essence')
+    }
+})
+
+// Stone Essence // Wood Splinter
+BlockEvents.rightClicked('kubejs:overgrown_wood_log', event => {
+    if(Item.of(event.item.id).hasTag('forge:pickaxes') && event.hand == 'main_hand') {
+        event.player.swing(event.hand, true)
+        event.player.damageHeldItem('main_hand', 1)
+        event.player.give('mysticalagriculture:stone_essence')
+    } else if (Item.of(event.item.id).hasTag('forge:axes') && event.hand == 'main_hand') {
+        if(Item.of(event.item.id).hasTag('forge:axes') && event.hand == 'main_hand') {
+            event.player.swing(event.hand, true)
+            event.player.damageHeldItem('main_hand', 1)
+            event.player.give('kubejs:wood_splinter')
+        
+        }
+    }
+})
+
+// Nether Quartz Essence
+BlockEvents.rightClicked('minecraft:stone', event => {
+    if(event.hand == 'main_hand' && event.player.getMainHandItem().id == 'minecraft:flint') {
+        event.player.swing(event.hand, true)
+        event.item.count--
+        event.item.count--
+        event.player.give('mysticalagriculture:nether_quartz_essence')
+    }
+})
+
+
+
+
+// Polished data card substrate
 BlockEvents.rightClicked('minecraft:grindstone', event => {
-    if(event.item != 'kubejs:rough_data_card_substrate') return
-    event.item.count--
-    event.player.giveInHand('kubejs:polished_data_card_substrate')
+    if(event.hand == 'main_hand' && event.player.getMainHandItem().id == 'kubejs:rough_data_card_substrate') {
+        event.player.swing(event.hand, true)
+        event.item.count--
+        event.player.give('kubejs:polished_data_card_substrate')
+    }
 })
--- kubejs/server_scripts/recipes.js
+++ kubejs/server_scripts/recipes.js
@@ -1,162 +1,263 @@
 ServerEvents.recipes(event => {
-    //material testing kit
-    event.shaped(
-        Item.of('kubejs:material_testing_kit', 2),
-        [
-          'ABA',
-          'CDE',
-          'AFA'
-        ],
-        {
-          A: 'minecraft:iron_ingot',
-          B: 'minecraft:diorite',
-          C: 'minecraft:calcite',
-          D: 'minecraft:granite',
-          E: 'minecraft:andesite',
-          F: 'minecraft:diorite'
-        }
-    )
+  
+  function defaultPlates(plate, ingot) {
+    event.recipes.create.pressing(plate, ingot)
+  }
 
-    //advanced Material Data Card
-    event.shaped(
-        Item.of('kubejs:advanced_material_data_card', 1),
-        [
-          '   ',
-          '   ',
-          ' A '
-        ],
-        {
-          A: 'kubejs:material_testing_kit'
-        }
-    )
+  //material testing kit
+  event.shaped(
+      Item.of('kubejs:material_testing_kit', 2),
+      [
+        'ABA',
+        'CDE',
+        'AFA'
+      ],
+      {
+        A: 'minecraft:iron_ingot',
+        B: 'minecraft:diorite',
+        C: 'minecraft:calcite',
+        D: 'minecraft:granite',
+        E: 'minecraft:andesite',
+        F: 'minecraft:diorite'
+      }
+  )
 
-    //Culinary Material Data Card
-    event.shaped(
-        Item.of('kubejs:culinary_data_card', 1),
-        [
-          '   ',
-          '   ',
-          ' A '
-        ],
-        {
-          A: 'kubejs:advanced_material_data_card'
-        }
-    )
+  //advanced Material Data Card
+  event.shaped(
+      Item.of('kubejs:advanced_material_data_card', 1),
+      [
+        '   ',
+        '   ',
+        ' A '
+      ],
+      {
+        A: 'kubejs:material_testing_kit'
+      }
+  )
 
-    //gem Data Card
-    event.shaped(
-        Item.of('kubejs:gem_data_card', 1),
-        [
-          '   ',
-          '   ',
-          ' A '
-        ],
-        {
-          A: 'kubejs:advanced_material_data_card'
-        }
-    )
+  //Culinary Material Data Card
+  event.shaped(
+      Item.of('kubejs:culinary_data_card', 1),
+      [
+        '   ',
+        '   ',
+        ' A '
+      ],
+      {
+        A: 'kubejs:advanced_material_data_card'
+      }
+  )
 
-    //bloody Data Card
-    event.shaped(
-        Item.of('kubejs:bloody_data_card', 1),
-        [
-          '   ',
-          '   ',
-          ' A '
-        ],
-        {
-          A: 'kubejs:advanced_material_data_card'
-        }
-    )
+  //gem Data Card
+  event.shaped(
+      Item.of('kubejs:gem_data_card', 1),
+      [
+        '   ',
+        '   ',
+        ' A '
+      ],
+      {
+        A: 'kubejs:advanced_material_data_card'
+      }
+  )
 
-    //Twilight Data Card
-    event.shaped(
-        Item.of('kubejs:twilight_data_card', 1),
-        [
-          '   ',
-          '   ',
-          ' A '
-        ],
-        {
-          A: 'kubejs:gem_data_card'
-        }
-    )
+  //bloody Data Card
+  event.shaped(
+      Item.of('kubejs:bloody_data_card', 1),
+      [
+        '   ',
+        '   ',
+        ' A '
+      ],
+      {
+        A: 'kubejs:advanced_material_data_card'
+      }
+  )
 
-    //Compressed Data Card
-    event.shaped(
-        Item.of('kubejs:compressed_data_card', 1),
-        [
-          '   ',
-          '   ',
-          ' A '
-        ],
-        {
-          A: 'kubejs:advanced_material_data_card'
-        }
-    )
+  //Twilight Data Card
+  event.shaped(
+      Item.of('kubejs:twilight_data_card', 1),
+      [
+        '   ',
+        '   ',
+        ' A '
+      ],
+      {
+        A: 'kubejs:gem_data_card'
+      }
+  )
+
+  //Compressed Data Card
+  event.shaped(
+      Item.of('kubejs:compressed_data_card', 1),
+      [
+        '   ',
+        '   ',
+        ' A '
+      ],
+      {
+        A: 'kubejs:advanced_material_data_card'
+      }
+  )
     
-    //Electricity Data Card
-    event.shaped(
-        Item.of('kubejs:electricity_data_card', 1),
-        [
-          '   ',
-          '   ',
-          ' A '
-        ],
-        {
-          A: 'kubejs:advanced_material_data_card'
-        }
-    )
+  //Electricity Data Card
+  event.shaped(
+      Item.of('kubejs:electricity_data_card', 1),
+      [
+        '   ',
+        '   ',
+        ' A '
+      ],
+      {
+        A: 'kubejs:advanced_material_data_card'
+      }
+  )
 
-    //nuclear Data Card
-    event.shaped(
-        Item.of('kubejs:nuclear_data_card', 1),
-        [
-          '   ',
-          '   ',
-          ' A '
-        ],
-        {
-          A: 'kubejs:electricity_data_card'
-        }
-    )
+  //nuclear Data Card
+  event.shaped(
+      Item.of('kubejs:nuclear_data_card', 1),
+      [
+        '   ',
+        '   ',
+        ' A '
+      ],
+      {
+        A: 'kubejs:electricity_data_card'
+      }
+  )
 
-    //astral Data Card
-    event.shaped(
-        Item.of('kubejs:astral_data_card', 1),
-        [
-          '   ',
-          '   ',
-          ' A '
-        ],
-        {
-          A: 'kubejs:electricity_data_card'
-        }
-    )
+  //astral Data Card
+  event.shaped(
+      Item.of('kubejs:astral_data_card', 1),
+      [
+        '   ',
+        '   ',
+        ' A '
+      ],
+      {
+        A: 'kubejs:electricity_data_card'
+      }
+  )
 
-    //storage Data Card
-    event.shaped(
-        Item.of('kubejs:storage_data_card', 1),
-        [
-          '   ',
-          '   ',
-          ' A '
-        ],
-        {
-          A: 'kubejs:advanced_material_data_card'
-        }
-    )
+  //storage Data Card
+  event.shaped(
+      Item.of('kubejs:storage_data_card', 1),
+      [
+        '   ',
+        '   ',
+        ' A '
+      ],
+      {
+        A: 'kubejs:advanced_material_data_card'
+      }
+  )
     
-    //EMC Data Card
-    event.shaped(
-        Item.of('kubejs:emc_data_card', 1),
-        [
-          '   ',
-          '   ',
-          ' A '
-        ],
-        {
-          A: 'kubejs:storage_data_card'
-        }
-    )
+  //EMC Data Card
+  event.shaped(
+      Item.of('kubejs:emc_data_card', 1),
+      [
+        '   ',
+        '   ',
+        ' A '
+      ],
+      {
+        A: 'kubejs:storage_data_card'
+      }
+  )
+
+  // Overgrown Wood Log
+  event.shaped(
+      Item.of('kubejs:overgrown_wood_log', 1),
+      [
+        'BBB',
+        'BAB',
+        'BBB'
+      ],
+      {
+        A: 'mysticalagriculture:dirt_essence',
+        B: '#minecraft:logs'
+      }
+  )
+
+  // Blood Altar
+  event.replaceInput(
+    { output: 'bloodmagic:altar' },
+    'minecraft:gold_ingot',
+    'minecraft:andesite'
+  )
+
+  // Sacrificial Knife
+  event.remove({ id: 'bloodmagic:sacrificial_dagger'})
+  event.shaped(
+    Item.of('bloodmagic:sacrificialdagger', 1),
+    [
+      'A B',
+      'AB ',
+      'AAA'
+    ],
+    {
+      A: 'minecraft:polished_granite',
+      B: 'mysticalagriculture:nether_quartz_essence'
+    }
+  )
+
+  // Iron Nugget
+  event.recipes.bloodmagic.altar('minecraft:iron_nugget', 'kubejs:wood_splinter').upgradeLevel(0).altarSyphon(100).consumptionRate(10).drainRate(10)
+
+  // Water Wheel
+  event.replaceInput(
+    { output: 'create:water_wheel' },
+    'create:shaft',
+    'create:andesite_casing'
+  )
+
+  // Crushing Wheel
+  event.remove({ id: 'create:mechanical_crafting/crushing_wheel'})
+  event.shaped(
+    Item.of('create:crushing_wheel', 1),
+    [
+      'ABA',
+      'BCB',
+      'ABA'
+    ],
+    {
+      A: 'create:andesite_alloy',
+      B: '#minecraft:planks',
+      C: 'create:andesite_casing'
+    }
+  )
+
+  // Lapis Dust
+  event.recipes.create.milling('thermal:lapis_dust', 'minecraft:lapis_lazuli')
+  
+  // Coke Bricks
+  event.replaceInput(
+    { output: 'immersiveengineering:cokebrick' },
+    'minecraft:clay_ball',
+    'kubejs:plate_blueish'
+  )
+  
+  // Blast Bricks
+  event.replaceInput(
+    { output: 'immersiveengineering:blastbrick' },
+    'minecraft:magma_block',
+    'immersiveengineering:cokebrick'
+  )
+  event.replaceInput(
+    { output: 'immersiveengineering:blastbrick' },
+    'minecraft:nether_brick',
+    'kubejs:plate_blueish'
+  )
+  
+  // Blueish
+  event.recipes.create.milling('thermal:iron_dust', 'minecraft:iron_ingot')
+  event.shapeless(
+    Item.of('kubejs:dust_blueish', 2),
+    [
+      '3x #forge:dusts/iron',
+      '2x thermal:lapis_dust'
+    ]
+  )
+  event.smelting('kubejs:ingot_blueish', 'kubejs:dust_blueish')
+  defaultPlates('kubejs:plate_blueish', 'kubejs:ingot_blueish')
 })
