e3818bff9f0f7c3e5bd7ee75719c356686a1d88a
Sagenseele <ezimmermann18@googlemail.com>
2025-04-19 02:34:30 +0200

    removed tooltip from crystal chamber

--- kubejs/client_scripts/misc.js
+++ kubejs/client_scripts/misc.js
@@ -4,7 +4,4 @@ JEIEvents.removeCategories(event => {
 
 ItemEvents.tooltip(event => {
   event.add(['kubejs:rough_data_card_substrate'], 'Right Click on a grindstone to get polished data card substrate')
-  
-  event.add(['mbd2:crystal_chamber'], 'The block in the middle of the multiblock decides, which recipe gets processed. Look in the JEI view for more information.')
-
 })
