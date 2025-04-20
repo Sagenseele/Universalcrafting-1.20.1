7caee13ecd12788aa9959eae07e49b83076ee3b0
Sagenseele <ezimmermann18@googlemail.com>
2025-04-20 16:59:36 +0200

    changed starterkit

--- kubejs/server_scripts/initial.js
+++ kubejs/server_scripts/initial.js
@@ -1,6 +1,6 @@
-PlayerEvents.loggedIn(event => {
-    if(!event.player.stages.has('base')) {
-        event.player.stages.add('base')
-        event.player.give('ftbquests:book')
-    }
-})
+//PlayerEvents.loggedIn(event => {
+//    if(!event.player.stages.has('base')) {
+//        event.player.stages.add('base')
+//        event.player.give('ftbquests:book')
+//    }
+//})
