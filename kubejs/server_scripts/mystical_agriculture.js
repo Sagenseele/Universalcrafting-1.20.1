371fa21af5c8fe0c0abacb9308359b48662f31fc
Sagenseele <ezimmermann18@googlemail.com>
2025-09-08 19:02:58 +0200

    gated mysticalagriculture

--- kubejs/server_scripts/mystical_agriculture.js
+++ kubejs/server_scripts/mystical_agriculture.js
@@ -15,7 +15,7 @@ ServerEvents.recipes((event) => {
     function mixer(fluid_output, fluid_output_amount, fluid_input, fluid_input_amount, item_inputs, energy) {
         var inputs = []
         item_inputs.forEach(item => {
-            inputs.push({"item": item.toString()})
+            inputs.push({ "item": item.toString() })
         })
         var recipe = {
             "type": "immersiveengineering:mixer",
@@ -36,23 +36,15 @@ ServerEvents.recipes((event) => {
     // Inferium Crystal
     squeezer('kubejs:inferium_sludge', 50, 'mysticalagriculture:inferium_essence', 2048)
     event.recipes.thermal.refinery([Fluid.of('kubejs:inferium_enriched_water', 200), '1x kubejs:crystal_inferium'], Fluid.of('kubejs:inferium_sludge', 500))
-    
-    // Prudentium Crystal
-    //(<liquid:solution_prudentium> * 100, <liquid:water> * 100, [<ore:essencePrudentium>, <ore:essencePrudentium>, <ore:essencePrudentium>, <ore:essencePrudentium>], 2048);
-    mixer('kubejs:prudentium_solution', 100, 'minecraft:water', 100, ['mysticalagriculture:prudentium_essence', 'mysticalagriculture:prudentium_essence', 'mysticalagriculture:prudentium_essence'], 2048)
-    event.recipes.thermal.chiller('kubejs:crystal_prudentium', [Fluid.of('kubejs:prudentium_solution', 250)])
 
-    //<mysticalagriculture:crafting:1> % 5, <liquid:solution_prudentium> * 250
-    event.recipes.thermal.refinery([Fluid.of('kubejs:inferium_enriched_water', 200), Item.of('1x kubejs:crystal_inferium').withChance(0.05)], Fluid.of('kubejs:inferium_sludge', 500))
-    
-    // Crystallizer (?)
-    //mods.nuclearcraft.crystallizer.addRecipe([<liquid:water_prudentium> * 200, <mysticalagriculture:crafting:1>]);    
-    event.recipes.thermal.chiller('mysticalagriculture:prudentium_essence', [Fluid.of('kubejs:prudentium_enriched_water', 200)])
+    // Prudentium Crystal
+    mixer('kubejs:prudiferium_solution', 100, 'kubejs:inferium_enriched_water', 50, ['mysticalagriculture:prudentium_essence', 'mysticalagriculture:prudentium_essence', 'mysticalagriculture:prudentium_essence'], 2048)
+    event.recipes.thermal.chiller('kubejs:crystal_prudentium', [Fluid.of('kubejs:prudiferium_solution', 250)])
 
     //Salt Mixer (?)
     //mods.nuclearcraft.salt_mixer.addRecipe([<liquid:water_prudentium> * 500, <liquid:water_inferium> * 500, <liquid:solution_prudiferium> * 1000]);
 
-    
+
     //event.remove({type:"mysticalagriculture:infusion"})
     //event.forEachRecipe({ type: "immersiveengineering:squeezer" }, recipe => {
     //    recipe = recipe.json.toString()
@@ -66,4 +58,31 @@ ServerEvents.recipes((event) => {
     // Supremium Crystal
 
     // Insanium Crystal
+
+    // Infusion Zeug
+    event.replaceInput(
+        { output: 'mysticalagriculture:infusion_altar' },
+        'minecraft:gold_ingot',
+        'kubejs:electrical_machine_casing'
+    )
+    event.replaceInput(
+        { output: 'mysticalagriculture:infusion_pedestal' },
+        'minecraft:gold_ingot',
+        'kubejs:electrical_machine_casing'
+    )
+    event.replaceInput(
+        { output: 'mysticalagriculture:awakening_altar' },
+        'minecraft:gold_ingot',
+        'kubejs:electrical_machine_casing'
+    )
+    event.replaceInput(
+        { output: 'mysticalagriculture:awakening_pedestal' },
+        'minecraft:gold_ingot',
+        'kubejs:electrical_machine_casing'
+    )
+    event.replaceInput(
+        { output: 'mysticalagriculture:essence_vessel' },
+        'minecraft:gold_ingot',
+        'kubejs:electrical_machine_casing'
+    )
 })
