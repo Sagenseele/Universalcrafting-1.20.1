4a415fc2995af086164745c8b8e76b3be190a98c
Erik Zimmermann <ezimmermann18@googlemail.com>
2025-11-02 23:15:30 +0100

    fixed egg incubaotr

--- kubejs/server_scripts/mbd2_egg_incubator.js
+++ kubejs/server_scripts/mbd2_egg_incubator.js
@@ -1,43 +1,31 @@
-let SpawnEggItem = Java.loadClass("net.minecraft.world.item.SpawnEggItem");
-let LootContext = Java.loadClass("net.minecraft.world.level.storage.loot.LootContext");
-let LootParams = Java.loadClass("net.minecraft.world.level.storage.loot.LootParams");
-let LootContextParamSets = Java.loadClass("net.minecraft.world.level.storage.loot.parameters.LootContextParamSets");
-let LootContextParams = Java.loadClass("net.minecraft.world.level.storage.loot.parameters.LootContextParams");
+const $LootParams$Builder = Java.loadClass("net.minecraft.world.level.storage.loot.LootParams$Builder")
+const $LootContextParams = Java.loadClass("net.minecraft.world.level.storage.loot.parameters.LootContextParams")
+const $LootContextParamSets = Java.loadClass("net.minecraft.world.level.storage.loot.parameters.LootContextParamSets")
 
-
-/*MBDMachineEvents.onBeforeRecipeModify('mbd2:egg_incubator', (event) => {
+MBDMachineEvents.onBeforeRecipeModify('mbd2:egg_incubator', (event) => {
     const mbdEvent = event.getEvent()
     const { machine, recipe } = mbdEvent;
 
     let itemInputTrait = machine.getTraitByName("item_slot");
-    let itemOutputTrait = machine.getTraitByName("item_slot_0");
-    if (itemInputTrait == null || itemOutputTrait == null) return;
+    if (itemInputTrait == null) return;
     let input = itemInputTrait.storage;
-    let output = itemOutputTrait.storage;
     let eggItem = input.getStackInSlot(0);
     if (eggItem.isEmpty()) return;
-    let builder = recipe.toBuilder();
-    builder.duration(20)
 
-    let level = machine.getLevel();
-    if (level == null) return;
-
+    let world = machine.getLevel();
+    if (world == null) return;
     let entityType = eggItem.getItem().getType(eggItem.nbt);
-    let lootTable = entityType.getDefaultLootTable();
-    new LootParams.Builder(level)
-        .withParameter(LootContextParams.THIS_ENTITY, entityType)
-        .withParameter(LootContextParams.ORIGIN, machine.getPos())
-        .create(lootTable)
+    let lootParamsBuilder = new $LootParams$Builder(world)
+            .withParameter($LootContextParams.THIS_ENTITY, entityType.create(world))
+            .withParameter($LootContextParams.ORIGIN, machine.getPos().getCenter())
+            .withParameter($LootContextParams.DAMAGE_SOURCE, world.damageSources().generic())
+    const lootParams = lootParamsBuilder.create($LootContextParamSets.ENTITY)
+    let lootTableId = entityType.getDefaultLootTable();
+    let lootTable = world.getServer().getLootData().getLootTable(lootTableId);
+    let loot = lootTable.getRandomItems(lootParams);
 
-
-
-    lootTable.forEach((itemStack) => {
-        for (let i = 0; i < output.getSlots(); i++) {
-            if (output.insertItem(i, itemStack, false).isEmpty()) break;
-        }
-    });
-
-    builder.outputItems("minecraft:iron_ingot"); //for debug
+    let builder = recipe.toBuilder();
+    builder.outputItems(loot)
     let newRecipe = builder.buildMBDRecipe();
     mbdEvent.setRecipe(newRecipe);
-})*/
+})
